445e34784aae184f7a20ba56b4b044f5b7eec1bd
MrWillCom <mr.will.com@outlook.com>
2021-01-16 17:21:11 +0800

    Add user config

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
-const CONFIG = {
+var defaultConfig = {
     PORT: 48534,
     SHOW_RECORD_URL: true,
     MONGODB_URI: 'mongodb://localhost:27017/fiber-search',
     RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN: 'http://localhost:3000',
 }
 
-module.exports = CONFIG
+// Initially, `./config.json` doesn't have any configuration properties, its content is `{}`.
+// If you want to edit the configuration, don't edit `defaultConfig`, you should edit `./config.json` instead to overwrite the defaults.
+const userConfig = require('./config.json')
+for (const i in userConfig) {
+    defaultConfig[i] = userConfig[i];
+}
+
+module.exports = defaultConfig
